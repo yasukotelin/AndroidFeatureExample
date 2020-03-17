9b7fde5df6cfe58657a58c3c9739c1c49aeb56fb
yasukotelin <31115673+yasukotelin@users.noreply.github.com>
2020-03-17 19:43:34 +0900

    Remove suspend from PaletteUseCase

--- usecase_palette/src/main/java/com/github/yasukotelin/usecase_palette/PaletteUseCase.kt
+++ usecase_palette/src/main/java/com/github/yasukotelin/usecase_palette/PaletteUseCase.kt
@@ -5,16 +5,16 @@ import android.graphics.Color
 import androidx.palette.graphics.Palette
 
 interface PaletteUseCase {
-    suspend fun getMutedColor(bitmap: Bitmap): Int
-    suspend fun getVibrantColor(bitmap: Bitmap): Int
+    fun getMutedColor(bitmap: Bitmap): Int
+    fun getVibrantColor(bitmap: Bitmap): Int
 }
 
 class PaletteUseCaseImpl : PaletteUseCase {
-    override suspend fun getMutedColor(bitmap: Bitmap): Int {
+    override fun getMutedColor(bitmap: Bitmap): Int {
         return Palette.from(bitmap).generate().getMutedColor(Color.BLACK)
     }
 
-    override suspend fun getVibrantColor(bitmap: Bitmap): Int {
+    override fun getVibrantColor(bitmap: Bitmap): Int {
         return Palette.from(bitmap).generate().getVibrantColor(Color.BLACK)
     }
 }
